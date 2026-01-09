edb785469545bfdbbfcbd1dfe11d2773c704411a
Brady Burgener <bradylbsee@gmail.com>
2026-01-08 17:52:32 -0700

    Week 01 Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string play = "y";
+
+        while (play.ToLower() == "y" || play.ToLower() == "yes" )
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+            int guess = -1;
+            int times = 0;
+
+            while (guess != number)
+            {
+                Console.Write("What is the magic number? ");
+                guess = int.Parse(Console.ReadLine());
+
+                times ++;
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"It took you {times} guesses to get the number.");
+            Console.Write("Would you like to play again (Y/N)? ");
+            play = Console.ReadLine();
+        }
     }
 }
