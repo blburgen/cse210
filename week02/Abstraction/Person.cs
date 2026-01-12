5d615222ff7829dc49196d1f007ff6da5f27bc56
Brady Burgener <bradylbsee@gmail.com>
2026-01-11 17:44:06 -0700

    completed week02 resume

---
+++ week02/Abstraction/Person.cs
@@ -0,0 +1,20 @@
+public class Person
+{
+    public string _givenName = "";
+    public string _familyName = "";
+
+    public Person()
+    {
+        
+    }
+
+    public void ShowEasternName()
+    {
+        Console.WriteLine($"{_familyName}, {_givenName}");
+    }
+
+    public void ShowWesternName()
+    {
+        Console.WriteLine($"{_givenName} {_familyName}");
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Senior Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2030;
+        job1._endYear = 2040;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "BYUI";
+        job2._startYear = 2026;
+        job2._endYear = 2030;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Brady Burgener";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
